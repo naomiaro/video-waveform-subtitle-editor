e45c7b67a6cc9e5aa4ebfe64b9d0d67ab26c1dee
Naomi Aro <naomiaro@gmail.com>
2017-11-08 16:25:40 +0000

    build assets locally

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+require('bootstrap/dist/css/bootstrap.min.css');
+require('font-awesome/css/font-awesome.min.css');
+
 var WaveformPlaylist = require('waveform-playlist');
+
+
+// ytdl https://www.youtube.com/watch?v=q9ANdC7ZFAI --quality 248 --output video.webm
+// ytdl https://www.youtube.com/watch?v=q9ANdC7ZFAI --quality 251 --output audio.webm
+// ffmpeg -i audio.webm -vn -acodec copy ./dist/251.ogg
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-  filename: "[name].[contenthash].css",
+  //filename: "[name].[contenthash].css",
+  filename: "app.css",
   disable: process.env.NODE_ENV === 'development'
 });
 
@@ -14,18 +15,24 @@ module.exports = {
   },
   devtool: 'source-map',
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: extractSass.extract({
-        use: [{
-          loader: 'css-loader' // translates CSS into CommonJS
-        }, {
-          loader: 'sass-loader' // compiles Sass to CSS
-        }],
-        // use style-loader in development
-        fallback: 'style-loader' // creates style nodes from JS strings
-      }),
-    }]
+    rules: [
+      {
+        test: /\.scss|\.css$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader' // translates CSS into CommonJS
+          }, {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }],
+          // use style-loader in development
+          fallback: 'style-loader' // creates style nodes from JS strings
+        }),
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      }
+    ]
   },
   plugins: [
     extractSass
