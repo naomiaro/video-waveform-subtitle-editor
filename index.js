a4c6c2d3ab4cf680c1266960e93750fa54199b74
Naomi Aro <naomiaro@gmail.com>
2017-11-22 13:43:46 +0000

    hacking in subtitles as first try

--- index.js
+++ index.js
@@ -1,161 +1,12 @@
+const srtParser = require('subtitles-parser');
+
 require('bootstrap/dist/css/bootstrap.min.css');
 require('font-awesome/css/font-awesome.min.css');
 require('waveform-playlist/styles/playlist.scss');
+require('./app.scss');
+
 
 var WaveformPlaylist = require('waveform-playlist');
-
-var notes = [
-  {
-   "begin": "0.000", 
-   "children": [], 
-   "end": "2.680", 
-   "id": "f000001", 
-   "language": "eng", 
-   "lines": [
-    "1"
-   ]
-  }, 
-  {
-   "begin": "2.680", 
-   "children": [], 
-   "end": "5.880", 
-   "id": "f000002", 
-   "language": "eng", 
-   "lines": [
-    "From fairest creatures we desire increase,"
-   ]
-  }, 
-  {
-   "begin": "5.880", 
-   "children": [], 
-   "end": "9.240", 
-   "id": "f000003", 
-   "language": "eng", 
-   "lines": [
-    "That thereby beauty's rose might never die,"
-   ]
-  }, 
-  {
-   "begin": "9.240", 
-   "children": [], 
-   "end": "11.920", 
-   "id": "f000004", 
-   "language": "eng", 
-   "lines": [
-    "But as the riper should by time decease,"
-   ]
-  }, 
-  {
-   "begin": "11.920", 
-   "children": [], 
-   "end": "15.280", 
-   "id": "f000005", 
-   "language": "eng", 
-   "lines": [
-    "His tender heir might bear his memory:"
-   ]
-  }, 
-  {
-   "begin": "15.280", 
-   "children": [], 
-   "end": "18.600", 
-   "id": "f000006", 
-   "language": "eng", 
-   "lines": [
-    "But thou contracted to thine own bright eyes,"
-   ]
-  }, 
-  {
-   "begin": "18.600", 
-   "children": [], 
-   "end": "22.800", 
-   "id": "f000007", 
-   "language": "eng", 
-   "lines": [
-    "Feed'st thy light's flame with self-substantial fuel,"
-   ]
-  }, 
-  {
-   "begin": "22.800", 
-   "children": [], 
-   "end": "25.680", 
-   "id": "f000008", 
-   "language": "eng", 
-   "lines": [
-    "Making a famine where abundance lies,"
-   ]
-  }, 
-  {
-   "begin": "25.680", 
-   "children": [], 
-   "end": "31.240", 
-   "id": "f000009", 
-   "language": "eng", 
-   "lines": [
-    "Thy self thy foe, to thy sweet self too cruel:"
-   ]
-  }, 
-  {
-   "begin": "31.240", 
-   "children": [], 
-   "end": "34.280", 
-   "id": "f000010", 
-   "language": "eng", 
-   "lines": [
-    "Thou that art now the world's fresh ornament,"
-   ]
-  }, 
-  {
-   "begin": "34.280", 
-   "children": [], 
-   "end": "36.960", 
-   "id": "f000011", 
-   "language": "eng", 
-   "lines": [
-    "And only herald to the gaudy spring,"
-   ]
-  }, 
-  {
-   "begin": "36.960", 
-   "children": [], 
-   "end": "40.680", 
-   "id": "f000012", 
-   "language": "eng", 
-   "lines": [
-    "Within thine own bud buriest thy content,"
-   ]
-  }, 
-  {
-   "begin": "40.680", 
-   "children": [], 
-   "end": "44.560", 
-   "id": "f000013", 
-   "language": "eng", 
-   "lines": [
-    "And tender churl mak'st waste in niggarding:"
-   ]
-  }, 
-  {
-   "begin": "44.560", 
-   "children": [], 
-   "end": "48.080", 
-   "id": "f000014", 
-   "language": "eng", 
-   "lines": [
-    "Pity the world, or else this glutton be,"
-   ]
-  }, 
-  {
-   "begin": "48.080", 
-   "children": [], 
-   "end": "53.240", 
-   "id": "f000015", 
-   "language": "eng", 
-   "lines": [
-    "To eat the world's due, by the grave and thee."
-   ]
-  }
-];
 
 var actions = [
   {
@@ -216,18 +67,12 @@ var playlist = WaveformPlaylist.init({
   container: document.getElementById("playlist"),
   timescale: true,
   state: 'select',
-  samplesPerPixel: 4096,
+  samplesPerPixel: 1500,
+  zoomLevels: [1500],
   colors: {
     waveOutlineColor: '#E0EFF1',
     timeColor: 'grey',
     fadeColor: 'black'
-  },
-  annotationList: {
-    annotations: notes,
-    controls: actions,
-    editable: true,
-    isContinuousPlay: false,
-    linkEndpoints: true
   }
 });
 
@@ -341,25 +186,6 @@ function updateTime(time) {
 
 updateSelect(startTime, endTime);
 updateTime(audioPos);
-
-
-
-/*
-* Code below sets up events to send messages to the playlist.
-*/
-// $container.on("click", ".btn-playlist-state-group", function() {
-//   //reset these for now.
-//   $('.btn-fade-state-group').addClass('hidden');
-//   $('.btn-select-state-group').addClass('hidden');
-
-//   if ($('.btn-select').hasClass('active')) {
-//     $('.btn-select-state-group').removeClass('hidden');
-//   }
-
-//   if ($('.btn-fadein').hasClass('active') || $('.btn-fadeout').hasClass('active')) {
-//     $('.btn-fade-state-group').removeClass('hidden');
-//   }
-// });
 
 $container.on("click", ".btn-annotations-download", function() {
   ee.emit("annotationsrequest");
@@ -503,9 +329,45 @@ $container.on("drop", ".track-drop", function(e) {
 
   var dropEvent = e.originalEvent;
 
-  for (var i = 0; i < dropEvent.dataTransfer.files.length; i++) {
-    ee.emit("newtrack", dropEvent.dataTransfer.files[i]);
-  }
+  // for (var i = 0; i < dropEvent.dataTransfer.files.length; i++) {
+  //   ee.emit("newtrack", dropEvent.dataTransfer.files[i]);
+  // }
+
+  // this.src.type.match(/video\/ogg/)
+  const subtitles = dropEvent.dataTransfer.files[0];
+  const fr = new FileReader();
+
+  fr.readAsText(subtitles);
+
+  fr.addEventListener('progress', (e) => {
+    const percentComplete = (e.loaded / e.total) * 100;
+  });
+
+  fr.addEventListener('load', (e) => {
+    const data = e.target.response || e.target.result;
+    const dataMs = srtParser.fromSrt(data, true);
+    const annotations = dataMs.map((subtitle) => {
+      return {
+        id: subtitle.id,
+        begin: subtitle.startTime / 1000,
+        end: subtitle.endTime / 1000,
+        lines: subtitle.text.split('\n')
+      };
+    });
+
+    playlist.setAnnotations({
+      annotations: annotations,
+      controls: actions,
+      editable: true,
+      isContinuousPlay: false,
+      linkEndpoints: true
+    });
+    playlist.drawRequest();
+  });
+
+  fr.addEventListener('error', (err) => {
+    console.error(err);
+  });
 });
 
 $container.on("change", ".time-format", function(e) {
