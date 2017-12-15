75fb3ba841161acbe2a26727f0b7a71684b44c16
Naomi Aro <naomiaro@gmail.com>
2017-12-14 20:30:01 -0800

    Get linked lib showing up.

--- Dockerfile
+++ Dockerfile
@@ -22,5 +22,6 @@ RUN npm install
 COPY . .
 
 EXPOSE 80
+
 # CMD [ "npm", "start" ]
 CMD [ "npm", "run", "dev" ]
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
   //filename: "[name].[contenthash].css",
-  filename: "app.css",
+  filename: 'app.css',
   disable: process.env.NODE_ENV === 'development'
 });
 
